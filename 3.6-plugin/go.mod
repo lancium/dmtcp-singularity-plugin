b402ebf8c89047eb2bf69f5a26b9110f9fc365b1
png <jasona99@outlook.com>
2020-06-03 13:12:04 -0400

    Initial work for 3.6 support

---
+++ 3.6-plugin/go.mod
@@ -0,0 +1,7 @@
+module dmtcp
+
+go 1.13
+
+require github.com/sylabs/singularity v0.0.0
+
+replace github.com/sylabs/singularity => ./singularity_source
---
+++ 3.6-plugin/main.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+	"github.com/sylabs/singularity/pkg/sylog"
+	"github.com/sylabs/singularity/pkg/cmdline"
+	"github.com/sylabs/singularity/pkg/runtime/engine/config"
+	singularity "github.com/sylabs/singularity/pkg/runtime/engine/singularity/config"
+	pluginapi "github.com/sylabs/singularity/pkg/plugin"
+)
+
+// Plugin is the only variable which a plugin MUST export.
+// This symbol is accessed by the plugin framework to initialize the plugin.
+var Plugin = pluginapi.Plugin{
+	Manifest: pluginapi.Manifest{
+		Name:        "lancium.com/dmtcp-singularity",
+		Author:      "Lancium",
+		Version:     "0.0.1",
+		Description: "This is a plugin to add checkpointing to Singularity with DMTCP",
+	},
+
+	Initializer: pluginImplementation{},
+}
+
+var(
+	BindPaths []string
+)
+
+type pluginImplementation struct{}
+
+func (p pluginImplementation) Initialize(r pluginapi.Registry) error {
+	isCheckpoint := false 
+	r.AddCLIMutator(pluginapi.CLIMutator{
+		Mutate: func(manager *cmdline.CommandManager) {
+			// create command: singularity checkpoint
+			var checkpointCmd = &cobra.Command{
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return errors.New("invalid command")
+				},
+				DisableFlagsInUseLine: false,
+				Use:           "checkpoint",
+				Short:         "Manage instances with checkpoint support",
+				Long:          "Manage instances with checkpoint support",
+				Example:       "...",
+				SilenceErrors: true,
+			}
+			
+			// register singularity checkpoint command
+			manager.RegisterCmd(checkpointCmd)
+			
+			//New scheme:
+			//singularity checkpoint [options for checkpointing] [instance] [command to run]
+			//this will translate roughly to singulairty exec [instance] /.dmtcp/bin/dmtcp_launch 
+			//[options] [command to run]
+			
+			// get reference to start Run method
+			instanceStartCmd := manager.GetCmd("instance_start")
+			if instanceStartCmd == nil {
+				sylog.Warningf("Could not find instance start command")
+				return
+			}
+			instanceStartCmdRun := instanceStartCmd.Run
+			
+			// create command: singularity checkpoint start
+			var checkpointStartCmd = &cobra.Command{
+				DisableFlagsInUseLine: true,
+				Args:                  cobra.MinimumNArgs(2),
+				Use:                   "start [args ...]",
+				Short:                 "Start an instance",
+				Long:                  "Start an instance with checkpoint capabilities",
+				Example:               "singularity checkpoint start <container> <name>",
+				Run: func(cmd *cobra.Command, args []string) {
+					isCheckpoint = true
+					//init checkpoint
+					//TODO: start a coordinator
+					instanceStartCmdRun(instanceStartCmd, args)
+				},
+				TraverseChildren: true,
+			}
+			// register checkpoint start command
+			manager.RegisterSubCmd(checkpointCmd, checkpointStartCmd)
+			checkpointStartCmd.Flags().AddFlagSet(instanceStartCmd.Flags())
+			
+			// get reference to exec Run method
+			execCmd := manager.GetCmd("exec")
+			if execCmd == nil {
+				sylog.Warningf("Could not find exec command")
+				return
+			}
+			execCmdRun := execCmd.Run
+			
+			
+			// get reference to stop Run method
+			instanceStopCmd := manager.GetCmd("instance_stop")
+			if instanceStopCmd == nil {
+				sylog.Warningf("Could not find instance stop command")
+				return
+			}
+			instanceStopCmdRun := instanceStopCmd.RunE
+			
+
+			var checkpointStopCmd = &cobra.Command{
+				DisableFlagsInUseLine: true,
+				Args:                  cobra.MinimumNArgs(1),
+				Use:                   "stop [args ...]",
+				Short:                 "Checkpoint, stop an instance",
+				Long:                  "Stop an instance with checkpoint capabilities after checkpointing",
+				Example:               "singularity checkpoint stop <name>",
+				Run: func(cmd *cobra.Command, args []string) {
+					isCheckpoint = true
+					//TODO: send a checkpoint command
+					//Checkpoint
+					newArgs := []string{"instance://"+args[0],"sh", "/.dmtcp/scripts/checkpoint_then_kill.sh"}
+					fmt.Println(newArgs)
+					ctkCmd := exec.Command("singularity", "exec", "instance://"+args[0], "sh", "/.dmtcp/scripts/checkpoint_then_kill.sh")
+					ctkCmd.Start()
+					ctkCmd.Wait()
+					fmt.Println("Shutting down instance...")
+					instanceStopCmdRun(instanceStopCmd, args)
+				},
+				TraverseChildren: true,
+			}
+			// register checkpoint stop command
+			manager.RegisterSubCmd(checkpointCmd, checkpointStopCmd)
+			checkpointStopCmd.Flags().AddFlagSet(instanceStopCmd.Flags())
+			
+			
+			// create command: singularity checkpoint exec
+			var checkpointExecCmd = &cobra.Command{
+				DisableFlagsInUseLine: true,
+				Args:                  cobra.MinimumNArgs(2),
+				Use:                   "exec [args ...]",
+				Short:                 "Execute a program in the instance",
+				Long:                  "Execute a program in the given instance with checkpoint",
+				Example:               "singularity checkpoint exec <name> <command>",
+				Run: func(cmd *cobra.Command, args []string) {
+					isCheckpoint = true
+					newArgs := []string{args[0],"sh", "/.dmtcp/scripts/launch.sh"}
+					newArgs = append(newArgs, args[1:]...)
+					fmt.Println(newArgs)
+					execCmdRun(execCmd, newArgs)
+				},
+				TraverseChildren: true,
+			}
+			// register checkpoint exec command
+			manager.RegisterSubCmd(checkpointCmd, checkpointExecCmd)
+			
+			// create command: singularity checkpoint run
+			var checkpointRunCmd = &cobra.Command{
+				DisableFlagsInUseLine: true,
+				Args:                  cobra.MinimumNArgs(2),
+				Use:                   "run [args ...]",
+				Short:                 "Execute the runscript in the instance",
+				Long:                  "Execute the runscript in the given instance with checkpoint",
+				Example:               "singularity checkpoint run <instance> <command>",
+				Run: func(cmd *cobra.Command, args []string) {
+					newArgs := []string{args[0],"sh", "/.dmtcp/scripts/launch.sh", "/singularity"}
+					fmt.Println(newArgs)
+					execCmdRun(execCmd, newArgs)
+				},
+				TraverseChildren: true,
+			}
+			// register checkpoint run command
+			manager.RegisterSubCmd(checkpointCmd, checkpointRunCmd)
+			
+			
+			// create command: singularity checkpoint running tasks
+			var checkpointMakeCmd = &cobra.Command{
+				DisableFlagsInUseLine: true,
+				Args:                  cobra.MinimumNArgs(1),
+				Use:                   "make [args ...]",
+				Short:                 "Checkpoint in the instance",
+				Long:                  "Checkpoint in the given instance",
+				Example:               "singularity checkpoint make <instance>",
+				Run: func(cmd *cobra.Command, args []string) {
+					newArgs := []string{args[0],"sh", "/.dmtcp/scripts/checkpoint.sh"}
+					fmt.Println(newArgs)
+					execCmdRun(execCmd, newArgs)
+				},
+				TraverseChildren: true,
+			}
+			// register checkpoint make command
+			manager.RegisterSubCmd(checkpointCmd, checkpointMakeCmd)
+			
+			// create command: singularity checkpoint restart task
+			var checkpointRestartCmd = &cobra.Command{
+				DisableFlagsInUseLine: true,
+				Args:                  cobra.MinimumNArgs(1),
+				Use:                   "restart [args ...]",
+				Short:                 "Restart from script",
+				Long:                  "Restart from checkpoint script in mounted checkpoint file",
+				Example:               "singularity checkpoint restart <instance>",
+				Run: func(cmd *cobra.Command, args []string) {
+					newArgs := []string{args[0],"sh", "/.dmtcp/scripts/restart.sh"}
+					fmt.Println(newArgs)
+					execCmdRun(execCmd, newArgs)
+				},
+				TraverseChildren: true,
+			}
+			// register checkpoint make command
+			manager.RegisterSubCmd(checkpointCmd, checkpointRestartCmd)
+			
+			// Ask for the checkpoint, wait for finish.
+			// TODO: Add script for checkpointing first, then run here.
+			
+		},
+	})
+	r.AddEngineConfigMutator(pluginapi.EngineConfigMutator{
+		Mutate: func(common *config.Common) {
+			c, ok := common.EngineConfig.(*singularity.EngineConfig)
+			if !ok {
+				sylog.Warningf("Unexpected engine config")
+				return
+			}
+			//Add bind for DMTCP if in environment.
+			if isCheckpoint{
+				dmtcpLocation := os.Getenv("SINGULARITY_DMTCP")
+				if(dmtcpLocation == ""){
+					sylog.Errorf("No DMTCP location found. Run install script?")
+					return 
+				}
+				origBind := c.GetBindPath()
+				c.SetBindPath(append(origBind, dmtcpLocation+":/.dmtcp/"))
+				//fmt.Println(c.GetBindPath())
+			}
+		},
+	})
+
+	return nil
+}
+
+
+
