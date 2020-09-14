9fa0147afa79a2d5ac9698f6a22b8de6afc490f0
png <jasona99@outlook.com>
2020-09-14 14:48:02 -0400

    Adjusted to support write-only filesystems

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Launching DMTCP..."
-mkdir -p ./lancium-checkpoint/
-/.dmtcp/dmtcp/bin/dmtcp_launch --ckptdir ./lancium-checkpoint/ --ckpt-open-files $@
+#/.dmtcp/dmtcp/bin/dmtcp_launch --join-coordinator --ckptdir ./lancium-checkpoint/ --ckpt-open-files $@
+/.dmtcp/dmtcp/bin/dmtcp_launch --ckptdir /.checkpoint/ --ckpt-open-files $@
